f6a74555b24d45fad7eac90a2cb5dcaa9877a7f0
Rural Innovation Tech Team <107579881+tech-team-rural-mda@users.noreply.github.com>
2024-06-20 13:02:56 -0400

    Fix USER AGENT header

--- R/dForm.R
+++ R/dForm.R
@@ -1,3 +1,9 @@
+# The SEC servers are returning an error when we use the default User-Agent for HTTPS requests...
+# Error in download.file("https://www.sec.gov/files/structureddata/data/form-d-data-sets/2020q1_d.zip",  : 
+#  cannot open URL 'https://www.sec.gov/files/structureddata/data/form-d-data-sets/2020q1_d.zip'
+# ... so we are hacking the User-Agent header that is sent with each request;
+options(HTTPUserAgent="Mozilla/5.0 (Macintosh; Intel Mac OS X 10.15; rv:128.0) Gecko/20100101 Firefox/128.0")
+
 #' R6 Class for downloading, caching, and basic processing of SEC Form D data
 #'
 #' @description
